2a26875980c918e5549a4059acba7bdc174a2e32
alexander-ponkratov <alexander.ponkratov.n@gmail.com>
2023-02-23 19:43:22 +0700

    materials 2

--- gulpfile.js
+++ gulpfile.js
@@ -8,8 +8,10 @@ const concat = require('gulp-concat')
 const autoprefixer = require('gulp-autoprefixer')
 const sync = require('browser-sync').create()
 
+//объединение html файлов
 function html() {
 	return src('src/**/**.html')
+		//задание префикса
 		.pipe(include({
 			prefix: '@@'
 		}))
@@ -17,25 +19,28 @@ function html() {
 		.pipe(dest('dist'))
 }
 
+//объединение scss файлов
 function scss() {
 	return src('src/scss/index.scss')
 		.pipe(sass())
-		//.pipe(autoprefixer())
+		//.pipe(autoprefixer()) //autoprefixer для совместимости
 		//.pipe(csso()) //minified css
 		.pipe(concat('index.css'))
 		.pipe(dest('dist'))
 }
 
+//шрифты
 function fonts() {
 	return src('src/fonts/**.*')
 		.pipe(dest('dist/fonts'))
 }
 
+//изображения
 function img() {
 	return src('src/parts/img/**.*')
 	.pipe(dest('dist/img'))
 }
-
+//очистка папки dist перед записью
 function clear() {
 	return del('dist')
 }
@@ -44,11 +49,13 @@ function serve() {
 	sync.init({
 		server: './dist'
 	})
+	//отслеживание изменений в папках и перезапись
 	watch('src/**.html', series(html)).on('change', sync.reload)
 	watch('src/parts/**.html', series(html)).on('change', sync.reload)
 	watch('src/scss/**.scss', series(scss)).on('change', sync.reload)
 	watch('src/parts/img/**.scss', series(img)).on('change', sync.reload)
 }
+//команды вида gulp build, gulp serve. Писать в консоли в корне проекта
 exports.build = series(clear, scss, html, fonts, img)
 exports.serve = series(clear, scss, html, fonts, img, serve)
 exports.clear = clear
